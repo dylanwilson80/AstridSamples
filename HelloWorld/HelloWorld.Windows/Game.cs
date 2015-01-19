3cce9d5e1664e6e5f6fe123953eb0ecf5c2e2848
craftworkgames <zarfius@gmail.com>
2015-01-20 08:30:18 +1000

    initial commit

---
+++ HelloWorld/HelloWorld.Windows/Game.cs
@@ -0,0 +1,42 @@
+﻿using Astrid.Core;
+using Astrid.Framework;
+
+namespace HelloWorld.Windows
+{
+    public class Game : GameBase
+    {
+        public Game(ApplicationBase application) 
+            : base(application)
+        {
+        }
+
+        public override void Create()
+        {
+        }
+
+        public override void Destroy()
+        {
+        }
+
+        public override void Pause()
+        {
+        }
+
+        public override void Resume()
+        {
+        }
+
+        public override void Resize(int width, int height)
+        {
+        }
+
+        public override void Update(float deltaTime)
+        {
+        }
+
+        public override void Render(float deltaTime)
+        {
+            GraphicsDevice.Clear(Color.SkyBlue);
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld.Windows/Program.cs
@@ -0,0 +1,23 @@
+﻿using Astrid.Windows;
+
+namespace HelloWorld.Windows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new WindowsApplicationConfig
+            {
+                Title = "Hello Astrid",
+                Width = 800,
+                Height = 480,
+            };
+
+            using (var application = new WindowsApplication(config))
+            {
+                var game = new Game(application);
+                application.Run(game);
+            }
+        }
+    }
+}
