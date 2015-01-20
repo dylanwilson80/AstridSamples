0d5b3d730d1b8653341b5703fc5285a3743f9c50
craftworkgames <zarfius@gmail.com>
2015-01-21 08:26:20 +1000

    completed a very basic hello world app

--- HelloWorld/HelloWorld.Windows/Program.cs
+++ HelloWorld/HelloWorld.Windows/Program.cs
@@ -4,13 +4,14 @@ namespace HelloWorld.Windows
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var config = new WindowsApplicationConfig
             {
                 Title = "Hello Astrid",
                 Width = 800,
                 Height = 480,
+                ContentPath = "Content"
             };
 
             using (var application = new WindowsApplication(config))
--- HelloWorld/HelloWorld/Game.cs
+++ HelloWorld/HelloWorld/Game.cs
@@ -1,10 +1,16 @@
 ﻿using Astrid.Core;
 using Astrid.Framework;
+using Astrid.Framework.Assets;
+using Astrid.Framework.Graphics;
 
 namespace HelloWorld
 {
     public class Game : GameBase
     {
+        private SpriteBatch _spriteBatch;
+        private Texture _texture;
+        private Vector2 _position;
+
         public Game(ApplicationBase application)
             : base(application)
         {
@@ -12,6 +18,13 @@ namespace HelloWorld
 
         public override void Create()
         {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _texture = AssetManager.Load<Texture>("AstridLogo.png");
+
+            // TODO: Get screen width and height from GraphicsDevice
+            var x = 400 - _texture.Width / 2;
+            var y = 240 - _texture.Height / 2;
+            _position = new Vector2(x, y);
         }
 
         public override void Destroy()
@@ -37,6 +50,10 @@ namespace HelloWorld
         public override void Render(float deltaTime)
         {
             GraphicsDevice.Clear(Color.SkyBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_texture, _position);
+            _spriteBatch.End();
         }
     }
 }
