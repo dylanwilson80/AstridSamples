f7bad6ef668b20b59b44a042011d8a8f6e2d4506
Dylan Wilson <dylan@craftworkgames.com>
2015-02-05 21:45:29 +1000

    created the hello world project for Android

---
+++ HelloWorld/HelloWorld.Android/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Astrid.Android;
+
+namespace HelloWorld.Android
+{
+    [Activity(Label = "HelloWorld.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private AndroidApplication _application;
+        private Game _game;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            Window.SetFlags(WindowManagerFlags.Fullscreen, WindowManagerFlags.Fullscreen);
+
+            var config = new AndroidApplicationConfig(this);
+            _application = new AndroidApplication(config);
+            _game = new Game(_application);
+            _application.Run(_game);
+            SetContentView(_application.View);
+        }
+
+        protected override void OnDestroy()
+        {
+            _application.Dispose();
+            base.OnDestroy();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            _application.Pause();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            _application.Resume();
+        }
+    }
+}
+
